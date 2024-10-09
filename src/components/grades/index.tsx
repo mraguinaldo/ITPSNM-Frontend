13f057fa231e435434afc3d8d09b4ec65845b27c
mraguinaldo <112706172+mraguinaldo@users.noreply.github.com>
2024-10-09 23:53:56 +0100

    Update index.tsx

--- src/components/grades/index.tsx
+++ src/components/grades/index.tsx
@@ -30,7 +30,7 @@ const Grades = () => {
 
   return (
     <section id='grade_report' className="pt-40 bg-white lg:bg-transparent">
-      <div className="w-full max-w-[1296px] flex flex-col gap-9 m-auto px-6 lg:p-11 lg:rounded-[16px] items-center pb-4">
+      <div className="w-full max-w-[1296px] flex flex-col gap-9 m-auto px-6 lg:p-11 lg:rounded-[16px] items-center pb-4 bg-white">
         <Header user={user} elementId='grade_report' />
         {showGrades ? (
           <TableContent notes={notes} error={error} isLoading={isLoading} />
